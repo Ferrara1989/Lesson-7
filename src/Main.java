b593266bd85909970742bb9cd16d1ecaef3e9cb3
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-12 16:55:33 +0300

    main, minAndMax, firstNumber, secondNumber

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int a = 0;
-    public static int b = 0;
+    public static int NumberEntry = 0;
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в программу вычисления наименьшего числа!");
-        System.out.println("Введите певое число: ");
-        firstNumber();
-        System.out.println("Введите второе число: ");
-        secondNumber();
-        minAndMax();
+       System.out.println("Добро пожаловать в программу печати числа в квадрате!");
+       System.out.println("Введите число: ");
+        numberEntry();
+        NumberInAaSquare();
     }
-    public static void minAndMax() {
-        if (b < a) {
-            System.out.println("Наименьшее число: " + b);
-        } else {
-            System.out.println("Наименьшее число: " + a);
+    public static void numberEntry() {
+        NumberEntry = new Scanner(System.in).nextInt();
+    }
+    public static void NumberInAaSquare() {
+        System.out.println("Число в квадрате: " + NumberEntry * 2);
         }
     }
-    public static void firstNumber() {
-        a = new Scanner(System.in).nextInt();
-    }
-    public static void secondNumber() {
-        b = new Scanner(System.in).nextInt();
-    }
-}
+
