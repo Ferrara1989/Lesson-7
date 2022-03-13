7f633185d7623b79a712895fb4d570c179ed10f1
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-13 20:48:39 +0300

    main, minAndMax, firstNumber, secondNumber

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String symbolWithOperation = "";
     public static int numberEntry = 0;
 
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в программа выдает true если число больше 5 и false если меньше или равно 5!");
+        System.out.println("Добро пожаловать в простой калькулятор на Java!");
+        calc(getInt(), getInt(), getOperation());
+    }
+
+    public static int getInt() {
         System.out.println("Введите число: ");
-        numberEntry();
-        rightAndWrong();
-    }
-
-    public static void numberEntry() {
         numberEntry = new Scanner(System.in).nextInt();
+        return numberEntry;
     }
 
-    public static void rightAndWrong() {
-        if (numberEntry > 5) {
-            System.out.println(true);
+    public static void calc(int num1, int num2, String symbolWithOperation) {
+        if (symbolWithOperation.equals("+")) {
+            System.out.println(num1 + num2);
+        } else if (symbolWithOperation.equals("-")) {
+            System.out.println(num1 - num2);
+        } else if (symbolWithOperation.equals("*")) {
+            System.out.println(num1 * num2);
+        } else if (symbolWithOperation.equals("/")) {
+        } else {
+            System.out.println("Нет такой операции!");
         }
-        if (numberEntry <= 5) {
-            System.out.println(false);
-        }
+    }
+
+    public static String getOperation() {
+        System.out.println("Введите символ операции: ");
+        symbolWithOperation = new Scanner(System.in).nextLine();
+        return symbolWithOperation;
     }
 }
-
-
-
-
 
