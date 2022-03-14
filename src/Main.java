b503ed51d35ce94b0fac8db831f408f825ac16f8
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-14 17:18:38 +0300

    main, minAndMax, firstNumber, secondNumber

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static String symbolWithOperation = "";
-    public static int numberEntry = 0;
-
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в простой калькулятор на Java!");
-        calc(getInt(), getInt(), getOperation());
-    }
+    print();
 
-    public static int getInt() {
-        System.out.println("Введите число: ");
-        numberEntry = new Scanner(System.in).nextInt();
-        return numberEntry;
-    }
 
-    public static void calc(int num1, int num2, String symbolWithOperation) {
-        if (symbolWithOperation.equals("+")) {
-            System.out.println(num1 + num2);
-        } else if (symbolWithOperation.equals("-")) {
-            System.out.println(num1 - num2);
-        } else if (symbolWithOperation.equals("*")) {
-            System.out.println(num1 * num2);
-        } else if (symbolWithOperation.equals("/")) {
-        } else {
-            System.out.println("Нет такой операции!");
-        }
     }
-
-    public static String getOperation() {
-        System.out.println("Введите символ операции: ");
-        symbolWithOperation = new Scanner(System.in).nextLine();
-        return symbolWithOperation;
+    public static void print() {
+        System.out.println("Привет, Мир!");
     }
 }
 
