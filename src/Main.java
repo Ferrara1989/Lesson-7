3470feb2f1ae14412746f7ed3401884b56fa78b3
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-14 22:20:40 +0300

    main, minAndMax, firstNumber, secondNumber

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String text = "";
     public static void main(String[] args) {
-    print();
-
-
+        getText();
+        print();
+    }
+    public static void getText() {
+        System.out.println("Введите текст: ");
+        text = new Scanner(System.in).nextLine();
     }
     public static void print() {
-        System.out.println("Привет, Мир!");
+        System.out.println("Ваш текст: " + text);
     }
 }
-
