0cb4df4e10e26478de7d8ca329ec56b8a98da7f9
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-15 20:52:21 +0300

    main, minAndMax, firstNumber, secondNumber

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int num1 = getNumber();
-        System.out.println("Введите второе число: ");
-        int num2 = getNumber();
-        System.out.println("Сумма " + num1 + "+" + num2 + " равна " + (num1 + num2));
-
+        print(userSurName(),userName(), userPatronymic(), DateOfBirth());
     }
-
-    public static int getNumber() {
+    public static String userSurName() {
+        System.out.println("Введите фамилию");
+        return new Scanner(System.in).nextLine();
+    }
+    public static String userName() {
+        System.out.println("Ввветие имя");
+        return new Scanner(System.in).nextLine();
+    }
+    public static String userPatronymic() {
+        System.out.println("Введите отчество");
+        return new Scanner(System.in).nextLine();
+    }
+    public static int DateOfBirth() {
+        System.out.println("Дату рождения");
         return new Scanner(System.in).nextInt();
     }
+    public static void print(String userSurName, String userName, String userPatronymic, int DateOfBirth) {
+        System.out.println("Фамилия" + userSurName);
+        System.out.println("Имя" + userName);
+        System.out.println("Отчество" + userPatronymic);
+        System.out.println("Дата рождения" + DateOfBirth);
+    }
 }
-
