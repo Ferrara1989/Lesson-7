00059ee1a8421f6ba5af6e8a8d96e9777b309bff
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-15 13:34:43 +0300

    main, minAndMax, firstNumber, secondNumber

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int number = 0;
-    public static int a = 0;
+
     public static void main(String[] args) {
-    a = getNumber();
-    results();
+        System.out.println("Введите первое число: ");
+        int num1 = getNumber();
+        System.out.println("Введите второе число: ");
+        int num2 = getNumber();
+        System.out.println("Сумма " + num1 + "+" + num2 + " равна " + (num1 + num2));
+
     }
+
     public static int getNumber() {
-    number = new Scanner(System.in).nextInt();
-    return number;
-    }
-    public static void results() {
-        if (a % 3 == 0) {
-            System.out.println("Не делится");
-        }
+        return new Scanner(System.in).nextInt();
     }
 }
+
