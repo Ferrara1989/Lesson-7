57d748957fec2042232adfa29ff038818d687d47
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-16 20:59:23 +0300

    main, minAndMax, firstNumber, secondNumber, temperature converter

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int age = 0;
+    public static int degreesСelsius = 0;
+    public static int degressFahrenheit = 0;
     public static void main(String[] args) {
-        System.out.println("Какой у Вас возраст?");
-        age = getAge();
-        print();
+        System.out.println("Добро пожаловать в конвертер температуры!");
+        print(getCelsus(), getFarengeit());
     }
-    public static int getAge() {
-        System.out.println("Введите свой возраст: ");
-        age = new Scanner(System.in).nextInt();
-        return age;
+    public static int getCelsus() {
+        System.out.println("Ввод градусов по Цельсию: ");
+        degreesСelsius = new Scanner(System.in).nextInt();
+        return degreesСelsius;
     }
-    public static void print() {
-        System.out.println("Ваш возраст: " + age);
+    public static int getFarengeit() {
+        System.out.println(degreesСelsius + " Цельсий (а) это " +
+                (degreesСelsius * 1.8 + 32) + " Фарингейт ");
+        degressFahrenheit = new Scanner(System.in).nextInt();
+        return degressFahrenheit;
+    }
+    public static void print(int getCelsus, int getFarengeit) {
+        System.out.println("Температура по Цельсию: " + getCelsus);
+        System.out.println("Температура по Фарингейту: " + getFarengeit);
     }
 }
